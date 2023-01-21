d3e9e2faa8aeb1f6d37146a49a36f798961f45a8
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-21 23:38:23 +0530

    queries=>solved

--- 2-select from world tutorial/2-select from world tutorial.sql
+++ 2-select from world tutorial/2-select from world tutorial.sql
@@ -40,4 +40,46 @@ select name,population,area from world
  where area > 3000000 or population > 250000000
 
 
+#8
+Show the countries that are big by area (more than 3 million) or big by population (more than 250 million) but not both. Show name, population and area.
 
+select name,population,area from world
+ where (area > 3000000 and population <=250000000)
+  OR (population > 250000000 and area <=3000000)
+  
+#9
+Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'.
+
+select name,round((population/1000000),2),
+ round((gdp/1000000000),2) from world
+  where continent = ('South America')
+  
+#10
+Show the name and per-capita GDP for those countries with a GDP of at least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+
+select name, round((gdp/population)/1000,0)*1000
+ from world where gdp > 1000000000000
+ 
+ #11
+ Show the name and capital where the name and the capital have the same number of characters.
+ 
+SELECT name,capital
+  FROM world
+ WHERE len(name) = len(capital)
+ 
+ #12
+ Show the name and the capital where the first letters of each match. Don''t include countries where the name and the capital are the same word.
+ 
+select name,capital from world where left(name,1) = left(capital,1)
+ and name <> capital
+ 
+ #13
+ Find the country that has all the vowels and no spaces in its name.
+ 
+ select name from world
+ where name like '%a%' and
+ name like '%e%' and 
+ name like '%i%'
+ and name like '%o%'
+ and name like '%u%'
+ and name not like '% %'
