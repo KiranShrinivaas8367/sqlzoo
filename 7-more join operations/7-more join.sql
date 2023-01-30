ac0c28a84225c755ead949900bfd7fd609685669
kiran shrinivaas <hayabuzakiran@gmail.com>
2023-01-30 10:58:11 +0530

    6-solved

--- 7-more join operations/7-more join.sql
+++ 7-more join operations/7-more join.sql
@@ -5,4 +5,37 @@ SELECT id, title
  FROM movie
  WHERE yr=1962
  
+#2
+Give year of 'Citizen Kane'.
+
+select yr from movie
+ where title = 'Citizen Kane'
+ 
+#3
+List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+
+select id,title,yr from movie
+ where title like 'Star Trek%'
+ order by yr
+ 
+#4
+What id number does the actor 'Glenn Close' have?
+
+select id from actor
+ where name = 'Glenn Close'
+ 
+#5
+What is the id of the film 'Casablanca'
+
+select id from movie
+ where title = 'Casablanca'
+ 
+#6
+Obtain the cast list for 'Casablanca'.
+
+select name from actor
+ join casting on actor.id = actorid
+ where movieid=27
+ 
+#7
 
